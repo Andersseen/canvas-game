76096ff88c463a4d3a8706ddfc773c70d2bf6e6a
Andrey <forestx24@gmail.com>
2021-07-12 18:55:20 +0200

    added asteroide rain

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,22 @@
 const canvas = document.querySelector('#game');
-
 const context = canvas.getContext('2d');
 
+const aster = [];
+let timer = 0;
 
-const asteroide = new Image();
-asteroide.src = "./assets/img/asteroide.png";
+
+const asteroideImg = new Image();
+asteroideImg.src = "./assets/img/asteroide.png";
 
 const backgroundImg = new Image();
 backgroundImg.src = "./assets/img/background.jpg";
 
-let aster = { x: 0, y: 300, dx: 1, dy: 2 };
+
+// aster.push({ x: 0, y: 300, dx: 10, dy: 20 });
 
 
-
-backgroundImg.onload = () => {
-    game()
+backgroundImg.onload = function () {
+    game();
 }
 
 //game loop
@@ -26,18 +28,33 @@ function game() {
 }
 
 function update() {
-    //fisics
-    aster.x = aster.x + aster.dx;
-    aster.y = aster.y + aster.dy;
 
-    //limits
-    if (aster.x >= 550 || aster.x < 0) aster.dx = -aster.dx;
-    if (aster.y >= 550 || aster.y < 0) aster.dy = -aster.dy;
+    timer++;
+    if (timer % 10 == 0) {
+        aster.push({
+            x: Math.random() * 600,
+            y: -50,
+            dx: Math.random() * 2 - 1,
+            dy: Math.random() * 2 + 2,
+        });
+    }
+
+    //fisics
+    for (i in aster) {
+        aster[i].x = aster[i].x + aster[i].dx;
+        aster[i].y = aster[i].y + aster[i].dy;
+
+        //limits
+        if (aster[i].x >= 550 || aster[i].x < 0) aster[i].dx = -aster[i].dx;
+        if (aster[i].y >= 600) aster.splice(i, 1);
+    }
+
 }
 
 function render() {
     context.drawImage(backgroundImg, 0, 0, 600, 600);
-    context.drawImage(asteroide, aster.x, aster.y, 50, 50);
+    for (i in aster)
+        context.drawImage(asteroideImg, aster[i].x, aster[i].y, 50, 50);
 }
 
 const requestAnimFrame = (function () {
