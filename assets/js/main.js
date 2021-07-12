7214b924f348d25d14b0f9114076259dd4c3c726
Andrey <forestx24@gmail.com>
2021-07-12 19:32:56 +0200

    added bullets

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,27 @@ const canvas = document.querySelector('#game');
 const context = canvas.getContext('2d');
 
 const aster = [];
+let fireball = [];
 let timer = 0;
-
+const starShip = { x: 300, y: 300 };
 
 const asteroideImg = new Image();
 asteroideImg.src = "./assets/img/asteroide.png";
 
+fireballImg = new Image();
+fireballImg.src = "./assets/img/fireball.png";
+
+const starShipImg = new Image();
+starShipImg.src = "./assets/img/starship.png";
+
 const backgroundImg = new Image();
-backgroundImg.src = "./assets/img/background.jpg";
+backgroundImg.src = "./assets/img/space_bg.jpg";
 
 
-// aster.push({ x: 0, y: 300, dx: 10, dy: 20 });
+canvas.addEventListener("mousemove", (e) => {
+    starShip.x = e.offsetX - 25;
+    starShip.y = e.offsetY - 13;
+})
 
 
 backgroundImg.onload = function () {
@@ -39,6 +49,22 @@ function update() {
         });
     }
 
+    //bullets
+    if (timer % 30 == 0) {
+        fireball.push({
+            x: starShip.x + 10,
+            y: starShip.y,
+            dx: 0,
+            dy: -5.2,
+        });
+        fireball.push({
+            x: starShip.x + 60,
+            y: starShip.y,
+            dx: 0,
+            dy: -5.2,
+        });
+    }
+
     //fisics
     for (i in aster) {
         aster[i].x = aster[i].x + aster[i].dx;
@@ -49,12 +75,27 @@ function update() {
         if (aster[i].y >= 600) aster.splice(i, 1);
     }
 
+    for (i in fireball) {
+        fireball[i].x = fireball[i].x + fireball[i].dx;
+        fireball[i].y = fireball[i].y + fireball[i].dy;
+
+        //limits
+        if (fireball[i].y < -30) fireball.splice(i, 1);
+    }
+
 }
 
 function render() {
     context.drawImage(backgroundImg, 0, 0, 600, 600);
-    for (i in aster)
+    context.drawImage(starShipImg, starShip.x, starShip.y, 100, 100);
+
+    for (i in fireball) {
+        context.drawImage(fireballImg, fireball[i].x, fireball[i].y, 30, 50);
+    }
+
+    for (i in aster) {
         context.drawImage(asteroideImg, aster[i].x, aster[i].y, 50, 50);
+    }
 }
 
 const requestAnimFrame = (function () {
